e0f1abcbc5c937f17f54745fef74513f74345622
Ramdhan Setiadhi <ramdhan.s@bts.id>
2023-12-11 20:15:50 +0700

    init

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("sql12.freesqldatabase.com:3306","sql12669503","NNdYUmGSH4","sql12669503");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
